a860d330937dc0430ad17cb63f1d3f90467d16c2
wwxiao09 <wenxiaowang@brandeis.edu>
2022-05-03 18:07:48 -0400

    Created a Mongoose Scheme

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const courses = require('./public/data/courses20-21.json')
 
 const mongoose = require('mongoose');
 // const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-const mongodb_URI = 'mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const mongodb_URI = 'mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongodb_URI = 'mongodb+srv://wwx09:tilly0421@cluster0.bbzwu.mongodb.net/test'
 
 mongoose.connect(mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // fix deprecation warnings
--- models/Food.js
+++ models/Food.js
@@ -4,3 +4,11 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 const Mixed = Schema.Types.Mixed;
 
+var foodSchema = Schema( {
+    restaurant: String,
+    foodName: String,
+    foodType: String,
+    calories: Number,
+} );
+
+module.exports = mongoose.model( 'Food', foodSchema );
