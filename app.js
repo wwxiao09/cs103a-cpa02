2cf7e9ab81f18ba8f3f6218b7e25167b7dad7703
wwxiao09 <wenxiaowang@brandeis.edu>
2022-05-06 12:53:56 -0400

    Fixed bugs

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ const dressings = require('./public/data/dressings.json')
 // *********************************************************** //
 
 const mongoose = require('mongoose');
-// const mongodb_URI = process.env.mongodb_URI
+const mongodb_URI = process.env.mongodb_URI
 // const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
 // const mongodb_URI = 'mongodb+srv://cs_sj:BrandeisSpr22@cluster0.kgugl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const mongodb_URI = 'mongodb+srv://wwx09:tilly0421@cluster0.bbzwu.mongodb.net/test'
+// const mongodb_URI = 'mongodb+srv://wwx09:tilly0421@cluster0.bbzwu.mongodb.net/test'
 
 mongoose.connect(mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 // fix deprecation warnings
--- startup.sh
+++ startup.sh
@@ -2,5 +2,6 @@
 export mongodb_URI='mongodb+srv://wwx09:tilly0421@cluster0.bbzwu.mongodb.net/test'
 
 echo "connecting to $mongodb_URI"
-npm install
-npm start
+nodemon
+# npm install
+# npm start
